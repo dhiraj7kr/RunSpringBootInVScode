f8489eb31256534437cd3860d593a509d0c6520c
DHIRAJ KUMAR <110246188+dhiraj7kr@users.noreply.github.com>
2025-01-12 18:14:21 +0530

    Add files via upload

---
+++ DhirajfirstApplication.java
@@ -0,0 +1,13 @@
+package com.dhirajfirstcode.dhirajfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhirajfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhirajfirstApplication.class, args);
+	}
+
+}
